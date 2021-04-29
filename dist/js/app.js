741aebd2083a740b9ea81beeed0b38837a7eb654
michele <micheleepedrotti@gmail.com>
2021-04-29 11:47:23 +0200

    navbar vue js dropdown menu

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,11 +7,38 @@
   \***********************/
 /***/ (() => {
 
+var navHomeData = {
+  'linkName': 'Home',
+  'dropdown': {
+    'col1': ['MaxCoach education', 'Course portal', 'Distant learning', 'Multimedia pedagogy', 'Modern Schooling', 'Remote Training', 'Health Coaching', 'Gym Coaching', 'Business'],
+    'col2': ['Artist', 'Kitchen Coach', 'Motivation', 'Dancing', 'Guitar', 'Yoga', 'Photography', 'Personal Finance'],
+    'img': 'homepages-mega-menu-image-alt.jpg'
+  }
+};
+var navLinksData = [{
+  'linkName': 'Pages',
+  'dropdown': ['link bla bla bla', 'link bla bla bla', 'link bla bla bla', 'link bla bla bla', 'link bla bla bla', 'link bla bla bla', 'link bla bla bla', 'link bla bla bla', 'link bla bla bla', 'link bla bla bla', 'link bla bla bla']
+}, {
+  'linkName': 'Courses',
+  'dropdown': ['link bla bla bla', 'link bla bla bla', 'link bla bla bla', 'link bla bla bla', 'link bla bla bla', 'link bla bla bla', 'link bla bla bla', 'link bla bla bla']
+}, {
+  'linkName': 'Features',
+  'dropdown': ['link bla bla bla', 'link bla bla bla']
+}, {
+  'linkName': 'Blog',
+  'dropdown': ['link bla bla bla', 'link bla bla bla', 'link bla bla bla', 'link bla bla bla']
+}, {
+  'linkName': 'Shop',
+  'dropdown': ['link bla bla bla', 'link bla bla bla', 'link bla bla bla', 'link bla bla bla', 'link bla bla bla']
+}];
+
 function initVue() {
   new Vue({
     el: "#app",
     data: {
-      scrollPosition: null
+      scrollPosition: null,
+      navHomeDropdown: navHomeData,
+      navLinksDropdown: navLinksData
     },
     mounted: function mounted() {
       window.addEventListener('scroll', this.getScrollPosition);
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,98 @@
+const navHomeData =  {
+
+    'linkName' : 'Home',
+    'dropdown' :{
+    
+        'col1' : [
+            'MaxCoach education',
+            'Course portal',
+            'Distant learning',
+            'Multimedia pedagogy',
+            'Modern Schooling',
+            'Remote Training',
+            'Health Coaching',
+            'Gym Coaching',
+            'Business'
+        ],
+        'col2' : [
+            'Artist',
+            'Kitchen Coach',
+            'Motivation',
+            'Dancing',
+            'Guitar',
+            'Yoga',
+            'Photography',
+            'Personal Finance'
+        ],
+        'img': 'homepages-mega-menu-image-alt.jpg'
+    }
+};
+
+const navLinksData = [
+
+    {
+        'linkName': 'Pages',
+        'dropdown': [
+
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+        ]
+    },
+    {
+        'linkName': 'Courses',
+        'dropdown': [
+
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+        ]
+    },
+    {
+        'linkName': 'Features',
+        'dropdown': [
+
+            'link bla bla bla',
+            'link bla bla bla',
+        ]
+    },
+    {
+        'linkName': 'Blog',
+        'dropdown': [
+
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+        ]
+    },
+    {
+        'linkName': 'Shop',
+        'dropdown': [
+
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+            'link bla bla bla',
+        ]
+    }
+];
+
+
 function initVue() {
 
     new Vue({
@@ -6,6 +101,9 @@ function initVue() {
         data:{
 
             scrollPosition: null,
+
+            navHomeDropdown: navHomeData,
+            navLinksDropdown: navLinksData,
         },
         mounted() {
             window.addEventListener('scroll', this.getScrollPosition);
