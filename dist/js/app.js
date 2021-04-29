c0e7807c3c4e2f5109cb189434398588063a58e6
michele <micheleepedrotti@gmail.com>
2021-04-29 13:52:12 +0200

    add vuejs to stories carousel

--- dist/js/app.js
+++ dist/js/app.js
@@ -31,6 +31,27 @@ var navLinksData = [{
   'linkName': 'Shop',
   'dropdown': ['link bla bla bla', 'link bla bla bla', 'link bla bla bla', 'link bla bla bla', 'link bla bla bla']
 }];
+var storiesData = [{
+  'quote': 'I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.',
+  'img': 'testimonial-avata-02.jpg',
+  'name': 'Mina Hollance',
+  'job': '/ Freelancer'
+}, {
+  'quote': 'I need to get a certification for English proficiency and MaxCoach is my best choice. Their tutors are smart and professionals when dealing with students',
+  'img': 'testimonial-avata-04.jpg',
+  'name': 'Modley Pondor',
+  'job': '/ IT Specialist'
+}, {
+  'quote': "I'm happy with thei arrangement of lessons and subjects. They reflect a scientific investigation into effective methods to adopt for learners.",
+  'img': 'testimonial-avata-01.jpg',
+  'name': 'Luvic Dubble',
+  'job': '/ Private Tutor'
+}, {
+  'quote': "I'm a very strict person so I require everything to be organized and neat. Then, I'll be able to make things right and shine. MaxCoach guys just got me.",
+  'img': 'testimonial-avata-03.jpg',
+  'name': 'Florence Themes',
+  'job': '/ Multimedia Admin'
+}];
 
 function initVue() {
   new Vue({
@@ -40,7 +61,9 @@ function initVue() {
       cursorX: null,
       cursorY: null,
       navHomeDropdown: navHomeData,
-      navLinksDropdown: navLinksData
+      navLinksDropdown: navLinksData,
+      stories: storiesData,
+      storyIndex: 0
     },
     mounted: function mounted() {
       // detect the page scroll
@@ -55,6 +78,20 @@ function initVue() {
       getMousePosition: function getMousePosition(event) {
         this.cursorX = event.pageX / 20;
         this.cursorY = event.pageY / 20 + 200;
+      },
+      changeStoryUp: function changeStoryUp() {
+        if (this.storyIndex < this.stories.length - 1) {
+          this.storyIndex++;
+        } else {
+          this.storyIndex = 0;
+        }
+      },
+      changeStoryDown: function changeStoryDown() {
+        if (this.storyIndex > 0) {
+          this.storyIndex--;
+        } else {
+          this.storyIndex = 3;
+        }
       }
     }
   });
--- src/js/app.js
+++ src/js/app.js
@@ -92,6 +92,33 @@ const navLinksData = [
     }
 ];
 
+const storiesData = [
+
+    {
+        'quote': 'I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.',
+        'img': 'testimonial-avata-02.jpg',
+        'name': 'Mina Hollance',
+        'job': '/ Freelancer'
+    },
+    {
+        'quote': 'I need to get a certification for English proficiency and MaxCoach is my best choice. Their tutors are smart and professionals when dealing with students',
+        'img': 'testimonial-avata-04.jpg',
+        'name': 'Modley Pondor',
+        'job': '/ IT Specialist'
+    },
+    {
+        'quote': "I'm happy with thei arrangement of lessons and subjects. They reflect a scientific investigation into effective methods to adopt for learners.",
+        'img': 'testimonial-avata-01.jpg',
+        'name': 'Luvic Dubble',
+        'job': '/ Private Tutor'
+    },
+    {
+        'quote': "I'm a very strict person so I require everything to be organized and neat. Then, I'll be able to make things right and shine. MaxCoach guys just got me.",
+        'img': 'testimonial-avata-03.jpg',
+        'name': 'Florence Themes',
+        'job': '/ Multimedia Admin'
+    }
+];
 
 function initVue() {
 
@@ -107,6 +134,9 @@ function initVue() {
 
             navHomeDropdown: navHomeData,
             navLinksDropdown: navLinksData,
+
+            stories: storiesData,
+            storyIndex: 0, 
         },
         mounted() {
             // detect the page scroll
@@ -123,6 +153,28 @@ function initVue() {
 
                 this.cursorX = (event.pageX / 20);
                 this.cursorY = ((event.pageY / 20) + 200);
+            },
+            changeStoryUp: function() {
+
+                if (this.storyIndex < this.stories.length - 1){
+
+                    this.storyIndex ++;
+                }
+                else{
+
+                    this.storyIndex = 0;
+                }
+            },
+            changeStoryDown: function () {
+
+                if (this.storyIndex > 0) {
+
+                    this.storyIndex--;
+                }
+                else {
+
+                    this.storyIndex = 3;
+                }
             },
         }
     });
